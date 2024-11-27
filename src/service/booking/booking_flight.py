7576fc80248f4d18b00dcf6d79cf0dab2b0857b2
heej-ng <spoa12@naver.com>
2024-11-27 20:07:51 +0900

    feat. booking할때 firestore insert

---
+++ src/service/booking/booking_flight.py
@@ -0,0 +1,17 @@
+from ...util.firebase import build_firestore_client
+
+def book_flight():
+    db = build_firestore_client()
+    collection_name = "test"
+    document_id = "doc1"
+    data = {
+        'name': 'John',
+        'age': 30,
+        'email': 'john@example.com'
+    }
+    
+    doc_ref = db.collection(collection_name).document(document_id)
+    doc_ref.set(data)
+    
+    print('데이터가 성공적으로 추가되었습니다.')
+    
--- src/service/flight_info/get_flight_info.py
+++ src/service/flight_info/get_flight_info.py
@@ -1,10 +1,11 @@
 from ...util.firebase import build_firestore_client
 
-# def get_flight_info():
-db = build_firestore_client()
-collection_name = "test"
-docs = db.collection(collection_name).stream()
-print(docs)
-for doc in docs:
-    print(f"{doc.id}: {doc.to_dict()}")
+def get_flight_info():
+    db = build_firestore_client()
+    collection_name = "test"
+    docs = db.collection(collection_name).stream()
+    print(docs)
+    for doc in docs:
+        print(f"{doc.id}: {doc.to_dict()}")
         
+get_flight_info
