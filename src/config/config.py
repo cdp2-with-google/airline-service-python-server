6bcb4de02dc83fdd3efa2ec7c51d0851822e7a91
MINSU LEE <minsoo030232@gmail.com>
2024-11-27 19:49:47 +0900

    add: config, oauth 코드 추가

---
+++ src/config/config.py
@@ -0,0 +1,5 @@
+GOOGLE_CLIENT_ID = "965998489425-b14jbag85kh6g0cog4ro3kd5jjf9lpql.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "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"
+GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
+
+FIREBASE_CREDENTIALS_PATH = "src/config/air-agent-5b0f5-firebase-adminsdk-k63ci-4474945e9b.json"
