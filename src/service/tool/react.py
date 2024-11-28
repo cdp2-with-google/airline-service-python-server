b8e7878d06cff948d66e4545ac3c76973956e79a
hw-ani <pcbmlh73@gmail.com>
2024-11-28 19:28:50 +0900

    Improve: 답변 성능 향상 및 설정 파일 변경
    - src/service/tool/react.py : 예제 추가, Instruction 정제 등 답변 성능 향상 작업
    - config.py 삭제하고 새로 설정 파일 세팅

--- src/service/tool/react.py
+++ src/service/tool/react.py
@@ -7,6 +7,7 @@ from vertexai.generative_models import (
     Tool,
 )
 from ..rag.vertex_ai_search import search_pdf
+from datetime import datetime
 
 def delcare_functions() -> Tool:
     # 항공사 운송 약관(탑승 수속, 수하물 규정 등)을 조회
@@ -16,7 +17,7 @@ def delcare_functions() -> Tool:
         parameters={
             "type": "object",
             "properties": {
-                "airline_name": {"type": "string", "description": "Airline name of the flight to be retrived about"},
+                "airline_name": {"type": "string", "description": "The name of the airline name of the policy to be retrieved."},
             },
             "required": ["airline_name"]
         },
@@ -31,7 +32,7 @@ def delcare_functions() -> Tool:
             "properties": {
                 "airport_name": {
                     "type": "string",
-                    "description": "The name of the airport to retrieve policy information for."
+                    "description": "The name of the airport name of the policy to be retrieved."
                 }
             },
             "required": ["airport_name"]
@@ -44,11 +45,15 @@ def delcare_functions() -> Tool:
         description="Retrieve flight detail informations",
         parameters={
             "type": "object",
-            # api대로 필요한 params 수정하기
             "properties": {
-                "airline_name": {"type": "string", "description": "Airline name of the flight to be retrived about"},
+                # "airline_name": {"type": "string", "description": "The name of the airline of the flight to be retrieved."},
+                "departure": {"type": "string", "description": "The name of the departure airport of the flight to be retrieved."},
+                "destination": {"type": "string", "description": "The name of the destination airport of the flight to be retrieved."},
+                "departure_code": {"type": "string", "description": "The code of the departure airport of the flight to be retrieved."},
+                "destination_code": {"type": "string", "description": "The code of the destination airport of the flight to be retrieved."},
+                "date": {"type": "string", "format": "date", "description": "The departure date of the flight."},
             },
-            "required": ["airline_name"]
+            "required": ["departure", "destination", "departure_code", "destination_code", "date"]
         },
     )
 
@@ -56,27 +61,16 @@ def delcare_functions() -> Tool:
     book_flight = FunctionDeclaration(
         name="book_flight",
         description="Book a flight requested by the user",
-        # 여기 parameters는 API에 따라 수정 필요
         parameters={
             "type": "object",
             "properties": {
-                "airline_name": {"type": "string", "description": "Airline name of the flight to be retrived about"},
+                # "airline_name": {"type": "string", "description": "The name of the airline for the flight to reserve."},
+                "departure": {"type": "string", "description": "The name of the departure airport for the flight to reserve."},
+                "destination": {"type": "string", "description": "The name of the destination airport for the flight to reserve."},
+                "date": {"type": "string", "format": "date", "description": "The departure date of the flight to reserve in format YYYY-MM-DD."},
+                "time": {"type": "string", "description": "The departure time of the flight to reserve in format HH:MM."},
             },
-            "required": ["airline_name"]
-        },
-    )
-    
-    # google calendar에서 비행 일정 조회
-    get_schedule_to_google_calendar = FunctionDeclaration(
-        name="add_schedule_to_google_calendar",
-        description="Look up the flight schedule requested by the user in Google Calendar",
-        # parameters는 API에 따라 수정 필요
-        parameters={
-            "type": "object",
-            "properties": {
-                "airline_name": {"type": "string", "description": "Airline name of the flight to be retrived about"},
-            },
-            "required": ["airline_name"]
+            "required": ["departure", "destination", "date", "time"]
         },
     )
 
@@ -87,7 +81,6 @@ def delcare_functions() -> Tool:
             # get_airport_policy,
             get_flight_information,
             book_flight,
-            # get_schedule_to_google_calendar
         ],
     )
     return flight_tool
@@ -96,54 +89,82 @@ def handle_airline_policy(prompt):
     return search_pdf(prompt)
 
 def handle_flight_information(prompt):
-    return "handle_flight_information is called."
+    return "handle_flight_information has been called."
+
+def handle_booking(prompt):
+    return "handle_booking has been called."
+
+operations = {
+    "get_airline_policy": handle_airline_policy,
+    "get_flight_information": handle_flight_information,
+    "book_flight": handle_booking
+}
 
 def send_chat_message(prompt:str) -> str:
     # (멘토님 추천) Gemini 활용 사전 질문 정제 필요? (ex. 나리타-도쿄 못 찾는 경우 등)
-
-    operations = {
-        "get_airline_policy": handle_airline_policy,
-        "get_flight_information": handle_flight_information
-    }
+    # 기존 context 유지 위해 대화 내역 가져와서 붙이는거 필요?
 
     flight_tool = delcare_functions()
-    system_instruction = """
-    system_instruction ="Analyze the user's request and process it using the appropriate function.
+    system_instruction = f"""
+    system_instruction ="Analyze the user's request and find the appropriate function and its all required properties.
     There are 3 functions, get_airline_policy, get_flight_information, and book_flight.
 
-    <example1>
+    <example-get_airline_policy>
     User prompt : Can I take a knife on board the Korean Air flight?
     Predicted Tool : flight_tool
     Predicted Function : get_airline_policy
-    Function parameters : {"airport_name": "Koeran Air"}
-    </example1>
-    <example2>
-    User prompt: Can you book a flight for me from San Francisco to Tokyo next Friday?
-    Predicted Tool: flight_tool
-    Predicted Function: book_flight
-    Function parameters: {"departure_city": "San Francisco", "destination_city": "Tokyo", "departure_date": "2024-12-01"}
-    </example2>
-    <example3>
-    User prompt: Are there any restrictions on carrying liquids on Qatar Airways?
-    Predicted Tool: flight_tool
-    Predicted Function: get_airline_policy
-    Function parameters: {"airline_name": "Qatar Airways"}
-    </example3>
-    <example4>
+    Function parameters : {{"airline_name": "Koeran Air"}}
+    </example-get_airline_policy>
+    <example-get_airline_policy>
     User prompt: What is the baggage allowance for Emirates economy class?
     Predicted Tool: flight_tool
     Predicted Function: get_airline_policy
-    Function parameters: {"airline_name": "Emirates"}
-    </exampl4>
-    <example5>
-    User prompt: What time does Delta flight DL123 arrive in New York?
+    Function parameters: {{"airline_name": "Emirates"}}
+    </example-get_airline_policy>
+
+    <example-get_flight_information>
+    User prompt: I want to know the schedule of the flights departing from Incheon to Tokyo.
+    Predicted Tool: None (Need more information because there is no exact date to be retrieved)
+    Predicted Function: None (Need more information because there is no exact date to be retrieved)
+    Answer: "Give me more informations about date."
+    </example-get_flight_information>
+    <example-get_flight_information>
+    User prompt: I want to know the schedule of the flights departing from Incheon Airport to Narita Airport tomorrow.
     Predicted Tool: flight_tool
     Predicted Function: get_flight_information
-    Function parameters: {"flight_number": "DL123", "destination": "New York"}
-    </example5>
+    Function parameters: {{"departure": "Incheon", "destination": "Narita", "departure_code": "ICN", "destination_code": "NRT", "date": "YYYY-MM-DD"}} (You must replace "date" with user provided date value)
+    </example-get_flight_information>
+    <example-get_flight_information>
+    User prompt: I want to know the schedule of the flights departing from Incheon to Tokyo tomorrow.
+    Predicted Tool: None (Need more information because there is no exact airport name to be retrieved)
+    Predicted Function: None (Need more information because there is no exact airport name to be retrieved)
+    Answer: "Near by tokyo there are several airports. Which one you want to use? Narita Airport or Haneda Airport?"
+    </example-get_flight_information>
 
-    If you are unsure which function to use, ask the user for more information in the user's language.
-    Don't answer the question directly, just find the appropriate function or ask about the additional information you need if you can't find the right function.
+    <example-book_flight>
+    User prompt: Can I book a flight from Narita to San Francisco?
+    Predicted Tool: None (Need more information because there is no exact date and time to reserve)
+    Predicted Function: None (Need more information because there is no exact date and time to reserve)
+    Answer: "Of course! When do you want to depart? Let me know exact date and time."
+    </example-book_flight>
+    <example-book_flight>
+    User prompt: Can I book a flight from Narita to San Francisco next Friday on 6PM?
+    Predicted Tool: flight_tool
+    Predicted Function: book_flight
+    Function parameters: {{"departure": "Narita", "destination": "San Francisco", "date": "YYYY-MM-DD", "time": "HH:MM"}} (You must replace date and time with user provided date and time value)
+    </example-book_flight>
+
+    <addtional information>
+    Date of today : {datetime.today().strftime('%Y-%m-%d')}
+    </additional information>
+
+    Follow below instruction strictly.
+    1. If the user prompt is enough to set up a function and put a value in the properties, don't ask any additional questions.
+    2. If you are unsure which function to use or properties to assign, ask the user for more information.
+        2-1. Don't answer the question directly, just (1)find the appropriate function or (2)ask about the additional information you need if you can't find the right function or properties in natural language.
+        2-2. Don't use or ask Airport IATA code when asking more information, use Airport name instead.
+        2-3. Don't ask about information you can infer.
+        2-4. Answer in the language of the user question.
     """
     
     model = GenerativeModel(
@@ -156,6 +177,7 @@ def send_chat_message(prompt:str) -> str:
 
     # Gemini로 채팅 메시지 보내기
     response = chat.send_message(prompt).candidates[0].content.parts[0]
+    print(response)
 
     # 함수 호출의 응답에서 값 추출
     if (response.function_call is not None):
