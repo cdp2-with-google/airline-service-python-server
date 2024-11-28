520f3f82964136e7f28089f1c2ae6a7a13da755d
MINSU LEE <minsoo030232@gmail.com>
2024-11-28 21:28:56 +0900

    feat: booking_flight 코드 수정

--- src/service/booking/booking_flight.py
+++ src/service/booking/booking_flight.py
@@ -1,16 +1,21 @@
 from ...util.firebase import init_firestore_client
 
-def book_flight():
+def book_flight(args):
     db = init_firestore_client()
-    collection_name = "test"
-    document_id = "doc1"
+    collection_name = "booking_flight"
     data = {
         'name': 'John',
         'age': 30,
-        'email': 'john@example.com'
+        'email': 'john@example.com',
+        'departure': args["departure"],
+        'departure_code': args["departure_code"],
+        'destination': args["destination"],
+        'destination_code': args["destination_code"],
+        'data': args["date"],
+        'time': args["time"]
     }
     
-    doc_ref = db.collection(collection_name).document(document_id)
+    doc_ref = db.collection(collection_name).document()
     doc_ref.set(data)
     
     print('데이터가 성공적으로 추가되었습니다.')
