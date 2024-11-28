74b0752c77d234b7a1dcc8fa906ab1c3b6635339
MINSU LEE <minsoo030232@gmail.com>
2024-11-28 23:18:56 +0900

    add: 게이트 번호, 좌석 번호, 클래스 랜덤 생성 코드 추가

--- src/service/booking/booking_flight.py
+++ src/service/booking/booking_flight.py
@@ -1,8 +1,19 @@
+import random
 from ...util.firebase import init_firestore_client
 
 def book_flight(args):
     db = init_firestore_client()
     collection_name = "booking_flight"
+
+    # 랜덤 게이트 번호 생성 (1 ~ 30)
+    gate = random.randint(1, 30)
+
+    # 랜덤 좌석 번호 생성 (1 ~ 30 + A ~ H)
+    seat_number = f"{random.randint(1, 30)}{random.choice('ABCDEFGH')}"
+
+    # 랜덤 클래스 선택 (이코노미, 비즈니스, 퍼스트)
+    seat_class = random.choice(['이코노미', '비즈니스', '퍼스트'])
+
     data = {
         'name': 'John',
         'age': 30,
@@ -11,12 +22,15 @@ def book_flight(args):
         'departure_code': args["departure_code"],
         'destination': args["destination"],
         'destination_code': args["destination_code"],
-        'data': args["date"],
-        'time': args["time"]
+        'date': args["date"],
+        'time': args["time"],
+        'gate': gate,
+        'seat_number': seat_number,
+        'seat_class': seat_class
     }
-    
+
+    # Firestore에 데이터 추가
     doc_ref = db.collection(collection_name).document()
     doc_ref.set(data)
-    
+
     print('데이터가 성공적으로 추가되었습니다.')
-    
