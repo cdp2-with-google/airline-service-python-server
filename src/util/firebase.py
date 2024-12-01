2300bc2ac63cd33e48e2dbeb5206c12b59d76ee5
hw-ani <pcbmlh73@gmail.com>
2024-12-01 22:03:11 +0900

    Fix: 중복 초기화 방지(firebase)

--- src/util/firebase.py
+++ src/util/firebase.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 def init_firestore_client():
-    cred = credentials.Certificate("resources/firestoreAccountKey.json")
-    firebase_admin.initialize_app(cred)
+    if not firebase_admin._apps:  # Firebase 앱이 초기화되지 않은 경우
+        cred = credentials.Certificate("resources/firestoreAccountKey.json")
+        firebase_admin.initialize_app(cred)
     return firestore.client()
-
